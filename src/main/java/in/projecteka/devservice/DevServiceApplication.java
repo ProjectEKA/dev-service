12efeceec39f3bd42c0fdd8c3e8b6ca8dcf5df95
mounicasagi <mounica6633@gmail.com>
2020-08-10 16:03:40 +0530

    [master] | Initial Commit.

---
+++ src/main/java/in/projecteka/devservice/DevServiceApplication.java
@@ -0,0 +1,11 @@
+package in.projecteka.devservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevServiceApplication.class, args);
+    }
+}
