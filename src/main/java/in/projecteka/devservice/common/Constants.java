a03cbe351bf564efaa1388522b6938404c15daa5
mounicasagi <mounica6633@gmail.com>
2020-08-11 20:01:15 +0530

    783_updateBridge_1 | adds version v1 to api paths /bridges & /heartbeat.

--- src/main/java/in/projecteka/devservice/common/Constants.java
+++ src/main/java/in/projecteka/devservice/common/Constants.java
@@ -1,6 +1,8 @@
 package in.projecteka.devservice.common;
 
 public class Constants {
-    public static final String PATH_BRIDGES = "/bridges";
-    public static final String PATH_HEARTBEAT = "/heartbeat";
+    public static final String API_VERSION = "v1";
+    public static final String CURRENT_VERSION = "/" + API_VERSION;
+    public static final String PATH_BRIDGES = CURRENT_VERSION + "/bridges";
+    public static final String PATH_HEARTBEAT = CURRENT_VERSION + "/heartbeat";
 }
