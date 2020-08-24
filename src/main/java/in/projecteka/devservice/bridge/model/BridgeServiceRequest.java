c750c1d76ddf099174538c5d37361244067acb8d
meghna <meghna.tanwal@thoughworks.com>
2020-08-24 09:36:43 +0530

    small change for error message

--- src/main/java/in/projecteka/devservice/bridge/model/BridgeServiceRequest.java
+++ src/main/java/in/projecteka/devservice/bridge/model/BridgeServiceRequest.java
@@ -17,7 +17,7 @@ public class BridgeServiceRequest {
     @NotNull(message = "service name alias can't be empty")
     List<String> alias;
     String city;
-    @NotNull(message = "Service type can't be empty")
+    @NotNull(message = "service type can't be empty")
     ServiceType type;
     boolean active;
 }
