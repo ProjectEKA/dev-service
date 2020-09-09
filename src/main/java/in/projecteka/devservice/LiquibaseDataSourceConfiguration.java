73c64b96eadf59a708eaab703920473ccc565135
Aditi <aggarwal.aditi97@gmail.com>
2020-09-09 18:56:14 +0530

    [940] Sangita/Aditi | Change LiquibaseDataSourceConfiguration

--- src/main/java/in/projecteka/devservice/LiquibaseDataSourceConfiguration.java
+++ src/main/java/in/projecteka/devservice/LiquibaseDataSourceConfiguration.java
@@ -1,7 +1,11 @@
 package in.projecteka.devservice;
 
+import com.zaxxer.hikari.HikariDataSource;
+import in.projecteka.devservice.support.SupportRequestService;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -26,19 +30,24 @@ class LiquibaseDataSourceProperties {
 
 @Configuration
 public class LiquibaseDataSourceConfiguration {
-
     @Autowired
     private in.projecteka.devservice.LiquibaseDataSourceProperties liquibaseDataSourceProperties;
+    private static final Logger logger = LoggerFactory.getLogger(LiquibaseDataSourceConfiguration.class);
 
     @LiquibaseDataSource
     @Bean
     public DataSource liquibaseDataSource() {
-
-        return DataSourceBuilder.create()
+        DataSource ds = DataSourceBuilder.create()
                 .username(liquibaseDataSourceProperties.getUsername())
                 .password(liquibaseDataSourceProperties.getPassword())
                 .url(liquibaseDataSourceProperties.getUrl())
                 .driverClassName(liquibaseDataSourceProperties.getDriverClassName())
                 .build();
+        if (ds instanceof HikariDataSource) {
+            ((HikariDataSource) ds).setMaximumPoolSize(1);
+            ((HikariDataSource) ds).setPoolName("Liquibase Pool");
+        }
+        logger.info("Initialized a datasource for {}", liquibaseDataSourceProperties.getUrl());
+        return ds;
     }
 }
